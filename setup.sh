712b6d174d5a952384b563eba9a765a777f8df5c
maschulze <public4me-01@t-online.de>
2024-01-01 21:57:27 +0100

    Setup script changed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nixos/disko-configuration.nix
 
-sudo nixos-generate-config --no-filesystems --root /mnt
+# sudo nixos-generate-config --no-filesystems --root /mnt
 
 sudo mv /tmp/nixos/configuration.nix /mnt/etc/nixos
 sudo mv /tmp/nixos/hardware-configuration.nix /mnt/etc/nixos
