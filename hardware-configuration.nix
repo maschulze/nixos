f7b444c31f9b0e9db4d584cb0930a87156734169
maschulze <public4me-01@t-online.de>
2024-01-07 17:21:54 +0100

    Updated

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,7 +5,7 @@
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
     "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
-    ./disko-configuration.nix
+    ./simple-efi.nix
   ];
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
